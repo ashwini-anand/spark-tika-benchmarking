24e763e4c0dbd4c1a07935666c987921f1dd4c5d
ashwini-anand <aayanand@gmail.com>
2017-03-22 11:04:12 +0530

    updating run-spark

--- run-spark-PdfCount.sh
+++ run-spark-PdfCount.sh
@@ -1,13 +1,13 @@
 source var.sh
 declare hdfs_input_dir="$1"
 declare hdfs_output_dir="$2"
-declare runs_per_folder = 4
+declare runs_per_folder=4
 
 for((i=0; i<size_length; i++))
 do
   for((j=0; j<count_length; j++))
   do
-    for((int k=0; k<runs_per_folder; k++))
+    for((k=0; k<$runs_per_folder; k++))
     do
       spark-submit --class FileDetectorHdfs --master yarn --deploy-mode cluster --driver-memory 12g --executor-memory 12g --executor-cores 1 --queue default jars/spark-tika.jar ${hdfs_input_dir}/pdf_${sizes[i]}kb_${counts[j]}_files/ ${hdfs_output_dir} 
       sleep 5
